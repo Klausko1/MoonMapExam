a7026ad6b426e85afe0a2a263955fa190e7c788c
Klausko1 <hermank.skoglund@hotmail.com>
2024-06-10 15:19:28 +0200

    Update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 export default {
-  base: "/MoonMapExam/",
   server: {
     // Set MIME types for specific file extensions
     mimeTypes: {
-      '.js': 'application/javascript',
+      ".js": "application/javascript",
     },
   },
 };
